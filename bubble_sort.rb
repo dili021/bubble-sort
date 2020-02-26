9721f7ff55569e633b1e55cc742047972e96234b
Stefan Dili <stefan.dili.021@gmail.com>
2020-02-26 21:57:04 +0100

    implemented TSE suggestions. print commands removed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,7 @@
 def bubble_sort_by(arr)
-  print 'Your array before sorting: ', arr, "\n\n"
   return arr if arr.size <= 1
 
   sorted = false
-  counter = 0
   until sorted
     sorted = true
     (arr.length - 1).times do |i|
@@ -12,22 +10,17 @@ def bubble_sort_by(arr)
         sorted = false
       end
     end
-    counter += 1
-    print "round #{counter} of sorting: ", arr, "\n\n"
   end
-  print 'Your array is now sorted!'
-  puts
+  arr
 end
 bubble_sort_by(%w[hello dfhadfhad asg asdgafhafdhadheh dasfhadfhdafhafghafh as]) do |left, right|
   left.length - right.length
 end
 
 def bubble_sort(arr)
-  print 'Your array before sorting: ', arr, "\n\n"
   return arr if arr.size <= 1
 
   sorted = false
-  counter = 0
   until sorted
     sorted = true
     (arr.length - 1).times do |i|
@@ -36,10 +29,7 @@ def bubble_sort(arr)
         sorted = false
       end
     end
-    counter += 1
-    print "round #{counter} of sorting: ", arr, "\n\n"
   end
-  print 'Your array is now sorted!'
-  puts
+  arr
 end
 bubble_sort([7, 3, 5, 15, 44, 12, 41, 66, 55])
